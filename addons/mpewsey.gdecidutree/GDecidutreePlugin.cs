7798bd5497161594eb61fe3ee635658e9ebf73de
Matt Pewsey <23442063+mpewsey@users.noreply.github.com>
2023-11-16 20:46:49 -0500

    Add C# plugin script

---
+++ addons/mpewsey.gdecidutree/GDecidutreePlugin.cs
@@ -0,0 +1,18 @@
+using Godot;
+
+namespace MPewsey.GDecidutree
+{
+    [Tool]
+    public partial class GDecidutreePlugin : EditorPlugin
+    {
+        public override void _EnterTree()
+        {
+            base._EnterTree();
+        }
+
+        public override void _ExitTree()
+        {
+            base._ExitTree();
+        }
+    }
+}
