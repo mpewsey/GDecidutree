0297df63e05e4dff30629a93433d0c3ddc4e712b
Matt Pewsey <23442063+mpewsey@users.noreply.github.com>
2023-12-10 16:59:32 -0500

    Update randomizer

--- addons/mpewsey.gdecidutree/scripts/runtime/Randomizer.cs
+++ addons/mpewsey.gdecidutree/scripts/runtime/Randomizer.cs
@@ -1,3 +1,4 @@
+using Godot;
 using System;
 using System.Collections.Generic;
 
@@ -5,12 +6,7 @@ namespace MPewsey.GDecidutree
 {
     public static class Randomizer
     {
-        public static Random Random { get; private set; } = new Random();
-
-        public static void SetSeed(int seed)
-        {
-            Random = new Random(seed);
-        }
+        private static Random Random { get; } = new Random();
 
         public static void Shuffle<T>(IList<T> list)
         {
@@ -21,9 +17,51 @@ namespace MPewsey.GDecidutree
             }
         }
 
+        public static List<T> Shuffled<T>(IEnumerable<T> collection)
+        {
+            var copy = new List<T>(collection);
+            Shuffle(copy);
+            return copy;
+        }
+
         public static bool ChanceSatisfied(double chance)
         {
-            return chance >= 1 || (chance > 0 && Random.NextDouble() <= chance);
+            return chance >= 1 || (chance > 0 && Range01() <= chance);
+        }
+
+        public static double Range01()
+        {
+            return Random.NextDouble();
+        }
+
+        public static int Range(int max)
+        {
+            return Random.Next(max);
+        }
+
+        public static int Range(int min, int max)
+        {
+            return Random.Next(min, max);
+        }
+
+        public static double Range(double max)
+        {
+            return max * Range01();
+        }
+
+        public static double Range(double min, double max)
+        {
+            var t = Range01();
+            return max * t + (1 - t) * min;
+        }
+
+        public static Vector2 InsideUnitCircle()
+        {
+            var radius = Math.Sqrt(Range01());
+            var angle = Range(2 * Math.PI);
+            var x = radius * Math.Cos(angle);
+            var y = radius * Math.Sin(angle);
+            return new Vector2((float)x, (float)y);
         }
     }
 }
